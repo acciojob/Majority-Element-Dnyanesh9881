3df518ecf2e01bbe81c2ed5956eb6ca7c24ca644
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2024-04-16 11:21:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your code here
+
+
+function findMajorityElement(nums) {
+    let candidate;
+    let count = 0;
+
+    for (let num of nums) {
+        if (count === 0) {
+            candidate = num;
+            count++;
+        } else if (candidate === num) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+
+    return candidate;
+}
+let arr=[2,1,2];
+console.log(finMajorityElement(arr))
